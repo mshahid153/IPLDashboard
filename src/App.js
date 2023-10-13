16f25e3316eb335f0a44770aa2a377beafd2caea
Shahid M <120563988+mshahid153@users.noreply.github.com>
2023-10-13 22:58:38 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {Route, Switch} from 'react-router-dom'
+import React from "react"
+
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import TeamMatches from './components/TeamMatches'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/team-matches/:id" component={TeamMatches} />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-route-container">
+    <h1 className="not-found-heading">Page Not Found !!</h1>
+  </div>
+)
+
+export default NotFound
